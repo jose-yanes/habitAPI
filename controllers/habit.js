50605372d1a4ffca30a178efb15b307df86974a8
Jose Yanes <113186145+jose-yanes@users.noreply.github.com>
2023-10-16 21:58:48 -0300

    Adding checker if habit or update exists

--- controllers/habit.js
+++ controllers/habit.js
@@ -4,8 +4,30 @@ const Update = require('../models/Update');
 const setHabit = async (req,res) =>{
     //agregar check if already exist
     req.body.createdBy = req.user.userId;
-    const habit = await Habit.create(req.body);
-    res.status(200).json({habit});
+    let habitTrue;
+
+    if(req.body.code){
+        console.log(`CODE ${req.body.code} used`)
+        habitTrue = await Habit.findOne({
+            createdBy: req.user.userId,
+            code: req.body.code
+        })
+    }else if(req.body.name){
+        console.log(`name ${req.body.name} used`)
+        habitTrue = await Habit.findOne({
+            createdBy: req.user.userId,
+            name: req.body.name
+        })
+    };
+
+    if(habitTrue){
+        console.log(`Habit ${habitTrue.name} already exists!`)
+        res.status(200).json({habitTrue,msg:`Habit was already created, no new habit needed/created`});
+        
+    }else{
+        const habit = await Habit.create(req.body);
+        res.status(200).json({habit})  
+    }
 }
 
 const createUpdate = async (req,res,habitName) => {
@@ -25,9 +47,20 @@ const createUpdate = async (req,res,habitName) => {
         status: req.body.status
     };
 
-    const updateHabit = await Update.create(habitObj);
+    const habitUpdate = await Update.findOneAndUpdate(
+        {name:habitName,date:req.body.date},
+        {quantity:req.body.quantity,status:req.body.status},
+        {new:true}
+    );
 
-    res.status(200).json({updateHabit});
+    if(habitUpdate){
+        console.log(`habit exist, updating`)
+        res.status(200).json({habitUpdate});
+    }else{
+        console.log(`no habit, creating`)
+        const updateHabit = await Update.create(habitObj);
+        res.status(200).json({updateHabit})
+    }
 }
 
 const habitStatus = async (req,res) => {
@@ -43,14 +76,10 @@ const habitStatus = async (req,res) => {
     if(habitTrue){
         createUpdate(req,res,habitTrue.name);
     }else{
-        console.log(`No habit with name: ${req.body.name} or code: ${req.body.code}`);
         res.status(400).send(`No habit with name: ${req.body.name} or code: ${req.body.code}`);
     }
-            //If no date provided, today will set as date
 
 };
-
-//GetHabitList
 
 module.exports = {
     setHabit,
